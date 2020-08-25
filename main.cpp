fbbd8dbeaf0d5d6e32babb17631deb06066e52cf
RacyH <69870252+RacyH@users.noreply.github.com>
2020-08-25 14:47:31 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+Racy Halterman
+C++ Fall 2020
+Due: August 31st
+Lab 1 Introduction to C++ Programming
+Write a code that makes "Hello World!" appear on the screen
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World! Blue";
+
+	return 0;
+}
